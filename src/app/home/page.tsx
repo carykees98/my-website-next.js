3c6e02f670c0e2f1d8dfbc51481ea9c1cc5f0cc2
carykees98 <trainboy980@gmail.com>
2022-11-16 11:11:05 -0500

    moved main page into a /home subdirectory

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  let currentPage: string = 'Home'
+
+  return (
+    <div className="justify-center max-w-3xl">
+      <div className="flex flex-grow text-white">
+        This is the body
+      </div>
+    </div>
+  )
+}
